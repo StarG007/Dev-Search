da56ec981bace984e0fa19e967ffdb4a71d4b482
StarG007 <97721555+StarG007@users.noreply.github.com>
2025-03-03 03:35:39 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+async function searchDev() {
+  let query = document.getElementById("searchInput").value;
+  if (!query) {
+    alert("Opps! you forget to enter dev name");
+    return;
+  }
+  let response = await fetch(
+    `https://api.github.com/search/users?q=${query}&per_page=10`
+  );
+  let data = await response.json();
+
+  let devDiv = document.getElementById("resultContainer");
+  devDiv.style.display = "block";
+
+  let resultDiv = document.getElementById("results");
+  resultDiv.innerHTML = "";
+
+  if (data.items && data.items.length > 0) {
+    data.items.forEach(async (devs) => {
+      let userDetails = await fetch(
+        `https://api.github.com/users/${devs.login}`
+      );
+      let userData = await userDetails.json();
+      console.log(userData);
+
+      let userCard = document.createElement("div");
+      userCard.classList.add("results-container");
+      userCard.innerHTML = `
+    <div class="devCard"> <div class="devDetails">
+    <div class="username">
+    <img
+          class="userimg" src="${devs.avatar_url}" alt="user image" width="84" height="auto"/>
+    <h3>${devs.login}<h3>
+    </div>
+    <div class="vertDivider"></div>
+    <div class="devinfo">
+    <div class="followers"><h4>${userData.followers}</h4><p>Followers</p></div>
+    <div class="followers"><h4>${userData.public_repos}</h4><p>Repositories</p></div>
+    </div>
+    </div></div>
+    `;
+      resultDiv.appendChild(userCard);
+    });
+  } else {
+    resultDiv.innerHTML = "<p>No users found.</p>";
+  }
+}
